ae23f20dddb05f43396610a531e379ea0406a9a0
lakshmikanth448 <132090524+lakshmikanth448@users.noreply.github.com>
2026-02-07 21:41:42 +0530

    Update HibernateUtil.java

--- portfolio-App/src/main/java/com/portfolio/util/HibernateUtil.java
+++ portfolio-App/src/main/java/com/portfolio/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.portfolio.util;
 
+import java.util.Properties;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -9,12 +10,35 @@ public class HibernateUtil {
 
     static {
         try {
-            sessionFactory = new Configuration()
-                    .configure("hibernate.cfg.xml")
-                    .buildSessionFactory();
-        } catch (Throwable ex) {
-            ex.printStackTrace();
-            throw new ExceptionInInitializerError(ex);
+            String host = System.getenv("MYSQLHOST");
+            String port = System.getenv("MYSQLPORT");
+            String db   = System.getenv("MYSQLDATABASE");
+            String user = System.getenv("MYSQLUSER");
+            String pass = System.getenv("MYSQLPASSWORD");
+
+            String jdbcUrl =
+                "jdbc:mysql://" + host + ":" + port + "/" + db +
+                "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+            Properties props = new Properties();
+            props.put("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            props.put("hibernate.connection.url", jdbcUrl);
+            props.put("hibernate.connection.username", user);
+            props.put("hibernate.connection.password", pass);
+            props.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+            props.put("hibernate.hbm2ddl.auto", "update");
+            props.put("hibernate.show_sql", "true");
+
+            Configuration cfg = new Configuration();
+            cfg.addProperties(props);
+            cfg.addAnnotatedClass(com.portfolio.model.User.class);
+            cfg.addAnnotatedClass(com.portfolio.model.Project.class);
+
+            sessionFactory = cfg.buildSessionFactory();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
         }
     }
 
@@ -22,4 +46,3 @@ public class HibernateUtil {
         return sessionFactory;
     }
 }
-
